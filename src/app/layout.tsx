df6764be81ad1c98b0049614d065f4f5edbe0c34
8 Hokage <56502279+OrtemkaDead@users.noreply.github.com>
2023-07-30 04:19:24 +0500

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Сдать гисту',
+  description: 'Сдайте гисту пж',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}): React.ReactElement {
+  return (
+    <html lang="ru">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
