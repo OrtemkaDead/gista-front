c8dc4c68caf03979be8f2932e4ac68439d7a3fc6
8 Hokage <56502279+OrtemkaDead@users.noreply.github.com>
2023-08-01 16:32:57 +0500

    MN-26 - карточка гисты + незаконченный компонент коллапса

---
+++ src/components/gista-card/index.tsx
@@ -0,0 +1,48 @@
+import classnames from 'classnames'
+import Image from 'next/image'
+
+import { Icon, Typography } from '@/ui-kit'
+
+import GistaImage from '@/assets/images/Intersect.png'
+
+import GistaCardProps from './gista-card.types'
+import './styles.scss'
+
+export const GistaCard: React.FC<GistaCardProps> = ({ className = '', editable = false }) => {
+  //* ClassNames
+  const componentClassName = 'gista-card'
+  const GistaCardClassName = classnames(
+    componentClassName,
+
+    className,
+  )
+
+  let iconsContent = <Icon iconName="star" />
+
+  if (editable) {
+    iconsContent = (
+      <>
+        <Icon iconName="edit" />
+        <Icon iconName="trash" />
+      </>
+    )
+  }
+
+  return (
+    <div className={GistaCardClassName}>
+      <Image
+        src={GistaImage}
+        alt="gista"
+      />
+      <div className={`${componentClassName}__content`}>
+        <Typography
+          className={`${componentClassName}__title`}
+          variant="headline-h2"
+        >
+          Мазок крови человека – окраска: азур II и эозин
+        </Typography>
+        <div className={`${componentClassName}__icons`}>{iconsContent}</div>
+      </div>
+    </div>
+  )
+}
