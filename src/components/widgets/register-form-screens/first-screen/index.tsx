973b1dfe71e14fd4f050d6884391c0f700bde043
Ivanov Mikhail <mishaivanov530@gmail.com>
2023-08-26 14:58:06 +0200

    MN-38 - логика первого скрина

--- src/components/widgets/register-form-screens/first-screen/index.tsx
+++ src/components/widgets/register-form-screens/first-screen/index.tsx
@@ -1,46 +1,92 @@
 'use client'
 
 import { Button, TextInput } from '@/components/shared'
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 import FirstScreenProps from './first-screen.types'
 import './styles.scss'
 
 export const FirstScreen: React.FC<FirstScreenProps> = ({ setActiveScreen }) => {
-  const [surname, setSurname] = useState<string>('')
-  const [name, setName] = useState<string>('')
-  const [lastName, setLastName] = useState<string>('')
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
 
   const componentClassName = 'first-screen'
 
-  const handleClick = () => {
+  const onSubmit = handleSubmit((data) => {
     setActiveScreen(2)
-  }
+  })
 
   return (
-    <form className={componentClassName}>
+    <form
+      onSubmit={onSubmit}
+      className={componentClassName}
+    >
       <div className={`${componentClassName}__inputs`}>
         <TextInput
-          value={surname}
-          setValue={setSurname}
+          register={register}
+          name="lastName"
           type="text"
           placeholder="Фамилия"
+          options={{
+            required: {
+              value: true,
+              message: `Поле "Фамилия" является обязательным`,
+            },
+            minLength: {
+              value: 2,
+              message: 'Должно быть не менее 2 символов',
+            },
+            pattern: {
+              message: 'Фамилия не валидна',
+              value: /^[А-ЯЁ][а-яё]*$/,
+            },
+          }}
+          hintText={errors.lastName?.message}
         />
         <TextInput
-          setValue={setName}
-          value={name}
+          register={register}
           type="text"
+          name="firstName"
           placeholder="Имя"
+          options={{
+            required: {
+              value: true,
+              message: `Поле "Имя" является обязательным`,
+            },
+            minLength: {
+              value: 2,
+              message: 'Должно быть не менее 2 символов',
+            },
+            pattern: {
+              message: 'Имя не валидно',
+              value: /^[А-ЯЁ][а-яё]*$/,
+            },
+          }}
+          hintText={errors.firstName?.message}
         />
         <TextInput
-          setValue={setLastName}
-          value={lastName}
+          register={register}
           type="text"
+          name="surname"
           placeholder="Отчество"
+          options={{
+            pattern: {
+              message: 'Отчество не валидно',
+              value: /^[А-ЯЁ][а-яё]*$/,
+            },
+            minLength: {
+              value: 2,
+              message: 'Должно быть не менее 2 символов',
+            },
+          }}
+          hintText={errors.surname?.message}
         />
       </div>
       <Button
-        onClick={handleClick}
+        as="submit"
         size="large"
         fullWidth
       >
