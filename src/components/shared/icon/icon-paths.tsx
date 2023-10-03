7ee16c53e0344a8a7bb56f73c814861eb3f9a0ab
Stepan Andreev <stepan10andreev@gmail.com>
2023-10-03 20:44:56 +0300

    fix icon

--- src/components/shared/icon/icon-paths.tsx
+++ src/components/shared/icon/icon-paths.tsx
@@ -308,9 +308,9 @@ const iconsPaths = {
       id="delete"
       d="M21 5.98C17.67 5.65 14.32 5.48 10.98 5.48C9 5.48 7.02 5.58 5.04 5.78L3 5.98M8.5 4.97L8.72 3.66C8.88 2.71 9 2 10.69 2H13.31C15 2 15.13 2.75 15.28 3.67L15.5 4.97M18.85 9.14L18.2 19.21C18.09 20.78 18 22 15.21 22H8.79C6 22 5.91 20.78 5.8 19.21L5.15 9.14M10.33 16.5H13.66M9.5 12.5H14.5"
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   ),
   clear: (
@@ -318,9 +318,9 @@ const iconsPaths = {
       id="clear"
       d="M7.64102 12.3587L12.3577 7.64199M12.3577 12.3587L7.64102 7.64199M9.99935 18.3337C14.5827 18.3337 18.3327 14.5837 18.3327 10.0003C18.3327 5.41699 14.5827 1.66699 9.99935 1.66699C5.41602 1.66699 1.66602 5.41699 1.66602 10.0003C1.66602 14.5837 5.41602 18.3337 9.99935 18.3337Z"
       stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   ),
 }
