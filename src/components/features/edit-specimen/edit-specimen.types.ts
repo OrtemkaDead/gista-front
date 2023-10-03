8ae5443c41b62342d50378f76cd0e8cf3811640d
Stepan Andreev <stepan10andreev@gmail.com>
2023-10-03 20:57:50 +0300

    MN-43 - fix types for EditSpecimenForm

--- src/components/features/edit-specimen/edit-specimen.types.ts
+++ src/components/features/edit-specimen/edit-specimen.types.ts
@@ -2,4 +2,7 @@
 import { CurriculumFormProps } from '@/components/shared/modal-forms/curriculum-form/curriculum-form.types'
 import { EditSpecimenFormProps } from '@/components/shared/modal-forms/edit-specimen-form/edit-specimen-form.types'
 
-export type EditSpecimenProps = Omit<EditSpecimenFormProps, 'onSubmit'>
+export type EditSpecimenProps = Omit<
+  EditSpecimenFormProps,
+  'onSubmit' | 'previewImage' | 'uploadImage'
+>
